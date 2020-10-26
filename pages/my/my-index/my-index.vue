4369d2373afb0573d744aa167f70b961296e79a1
Cerbur <1051140642@qq.com>
2020-10-26 15:22:15 +0800

    版本号显示

--- pages/my/my-index/my-index.vue
+++ pages/my/my-index/my-index.vue
@@ -41,7 +41,10 @@
 			</button>
 		</view>
 		<!-- <view class="text-lg text-blue" @tap="toGrade">{{ lastestGrade }}</view> -->
-	</view>
+        <view class="flex-row">
+            <text class="m-center text-center text-gray mt-5">v1.5.0@gdutday</text>
+        </view>
+    </view>
 </template>
 
 <script>
